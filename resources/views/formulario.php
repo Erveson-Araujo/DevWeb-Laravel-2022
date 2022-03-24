7d475cba03485bd471d9b06abbfed255b11b7d1f
FRANCISCO ARAUJO <202110066111@alunos.estacio.br>
2022-03-23 21:23:12 -0300

    Segundo Envio

---
+++ resources/views/formulario.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matrícula</title>
+
+<!-- https://www.w3schools.com/html/html_form_input_types.asp -->
+
+</head>
+<body>
+    <h1>Desenvolvimento web 2022.1</h1>
+    <h3>Matrícula</h3>
+    <form action="">
+        <label for="matricula">Matricula: </label>
+        <input type="text" id="matricula" name="matricula" placeholder="Digite sua matricula">
+        <br><br>
+        <label for="matricula">Nome: </label>
+        <input type="text" id="nome" name="nome" placeholder="Nome completo">
+        <br><br>
+        <label for="matricula">Data de nascimento: </label>
+        <input type="date" id="data" name="data de nascimento" placeholder="Data de nascimento">
+        <br><br>
+        <label for="matricula">Telefone: </label>
+        <input type="tel" id="telefone" name="telefone" placeholder="Telefone">
+        <br><br>
+        <label for="matricula">Email: </label>
+        <input type="email" id="email" name="email" placeholder="Email">
+        <br><br>
+        <label for="matricula">Senha: </label>
+        <input type="password" id="password" name="senha" placeholder="Senha">
+    </form>
+</body>
+</html>
