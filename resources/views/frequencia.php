6be2ec6b46258ebc36a7a7e10c91e8fe52e7217a
FRANCISCO ARAUJO <202110066111@alunos.estacio.br>
2022-03-30 21:03:46 -0300

    Terceiro envio

---
+++ resources/views/frequencia.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frequências</title>
+</head>
+<body>
+    <h1>Desenvolvimento web 2022.1</h1>
+    <h3>Frequências</h3>
+    <a href="welcome.blade.php"><button>Ir para Notas</button></a>
+    <table border="black">
+        <br>
+        <br>
+        <thead>
+            <tr>
+                <th>Matricula</th>
+                <th>Aluno</th>
+                <th>AULA 1</th>
+                <th>AULA 2</th>
+                <th>AULA 3</th>
+                <th>AULA 4</th>
+                <th>AULA 5</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>000001</td>
+                <td>Erveson Araújo</td>
+                <td>P</td>
+                <td>P</td>
+                <td>P</td>
+                <td>P</td>
+                <td>P</td>
+            </tr>
+            <tr>
+                <td>000002</td>
+                <td>Miqueias Maia</td>
+                <td>P</td>
+                <td>P</td>
+                <td>P</td>
+                <td>F</td>
+                <td>F</td>
+            </tr> 
+            <tr>
+                <td>000003</td>
+                <td>Iara Barbosa</td>
+                <td>F</td>
+                <td>F</td>
+                <td>F</td>
+                <td>F</td>
+                <td>F</td>
+            </tr> 
+            <tr>
+                <td>000004</td>
+                <td>Taís Moura</td>
+                <td>F</td>
+                <td>P</td>
+                <td>P</td>
+                <td>P</td>
+                <td>P</td>
+            </tr> 
+        </tbody>
+    </table>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notas</title>
+
+    <link rel="stylesheet" type="text/css" href="../css/index.css" />
+    
+    <style>
+        td{
+            text-align:center;
+        }
+    </style>
+
+    
+
+</head>
+<body id="page-top">
+    <nav class="navbar" id="page-header">
+        <h1 class="titulos" style="margin-left: 15px;">Desenvolvimento web <br>2022.1</h1>
+    </nav>
+    <div id="page-sub-header" style="border: 1px #000000 solid; text-align: center;"> 
+        <h2 class="textos-site">Notas</h2>
+    </div>
+    <div class="formulario" id="table">
+        <table border="black" align="center">
+            <br>
+            <br>
+            <thead>
+                <tr>
+                    <th>Matricula</th>
+                    <th>Aluno</th>
+                    <th>AV1</th>
+                    <th>AV2</th>
+                    <th>AVD</th>
+                    <th>AV3</th>
+                    <th>AVDS</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>000001</td>
+                    <td>Erveson Araújo</td>
+                    <td>10</td>
+                    <td>10</td>
+                    <td>10</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr>
+                <tr>
+                    <td>000002</td>
+                    <td>Miqueias Maia</td>
+                    <td>10</td>
+                    <td>9</td>
+                    <td>10</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr> 
+                <tr>
+                    <td>000003</td>
+                    <td>Iara Barbosa</td>
+                    <td>10</td>
+                    <td>9</td>
+                    <td>8</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr> 
+                <tr>
+                    <td>000004</td>
+                    <td>Taís Moura</td>
+                    <td>7</td>
+                    <td>10</td>
+                    <td>9</td>
+                    <td>-</td>
+                    <td>-</td>
+                </tr> 
+            </tbody>
+        </table>
+    </div>
+    <div id="button" style="margin-top: 20px;">
+    <a href="frequencia.php"><button class="btn-button">Ir para Frequências</button></a>
+    </div>
+</body>
+</html>
